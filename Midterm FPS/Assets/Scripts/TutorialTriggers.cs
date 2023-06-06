afa0eeb96fd2c925d457a6a59d6d28869c120964
Bjorn Nordenflycht <bjornnorden17@gmail.com>
2023-06-06 00:10:57 -0400

    Tutorial second room work

--- Midterm FPS/Assets/Scripts/TutorialTriggers.cs
+++ Midterm FPS/Assets/Scripts/TutorialTriggers.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class TutorialTriggers : MonoBehaviour
 {
     [SerializeField] Animator myDoor = null;
+    //[SerializeField] Animator girl = null;
 
     public GameObject firstUI;
     public GameObject secondUI;
     public GameObject firstTrigger;
     public GameObject secondTrigger;
+    public GameObject thirdTrigger;
     
     void OnTriggerEnter(Collider other)
-    { 
+    {
         if (gameObject.Equals(firstTrigger))
         {
             firstUI.SetActive(false);
             secondUI.SetActive(true);
         }
-        else if (gameObject.Equals(secondTrigger) && gameManager.instance.getEnemiesRemaining() <= 0) 
+        else if (gameObject.Equals(secondTrigger) && gameManager.instance.getEnemiesRemaining() <= 0)
         {
-                myDoor.Play("DoorOpen", 0, 0f);
-                secondTrigger.SetActive(false);
+            myDoor.Play("DoorOpen", 0, 0f);
+            secondTrigger.SetActive(false);
+        }
+        else if (gameObject.Equals(thirdTrigger))
+        {
+            myDoor.Play("DoorClosed", 0, 0f);
+            thirdTrigger.SetActive(false);
         }
     }
 }
