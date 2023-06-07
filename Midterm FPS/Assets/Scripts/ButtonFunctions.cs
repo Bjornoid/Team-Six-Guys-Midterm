d43af7d6e4e5c396314bbc0daadfeee969976319
Bjorn Nordenflycht <bjornnorden17@gmail.com>
2023-06-07 16:05:01 -0400

    changing tutorial

--- Midterm FPS/Assets/Scripts/ButtonFunctions.cs
+++ Midterm FPS/Assets/Scripts/ButtonFunctions.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
     public void settings()
     {
         gameManager.instance.switchToSettings();
+    }
+
+    public void play()
+    {
+        gameManager.instance.switchToLevelSelect();
+    }
+
+    public void tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void floor1()
+    {
+
+    }
+
+    public void floor2()
+    {
+
+    }
+    public void floor3()
+    {
+
+    }
+    public void floor4()
+    {
+
     }
 
     public void close()
