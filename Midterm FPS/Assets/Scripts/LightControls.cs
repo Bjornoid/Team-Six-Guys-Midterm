3bf08a873e8380c53db7fede2d4004636247a36c
Juancarlitoss <JCLillo@student.fullsail.edu>
2023-06-07 00:08:59 -0400

    Light Controls Script with Flicker
    Created a light controls script in order to control lights and make them flicker.

---
+++ Midterm FPS/Assets/Scripts/LightControls.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControls : MonoBehaviour
+{
+    [Header("----- Light Controls -----")]
+    [SerializeField] float flickerDelay; // change to edit delay between flickers
+
+    [Header("----- Flicker Delay Range")]
+    [Range(0, 20)][SerializeField] float rangeX;
+    [Range(0, 20)][SerializeField] float rangeY;
+
+    bool isFlickering; // checks to see if light is flickering
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFlickering)
+        {
+            StartCoroutine(Flicker());
+        }
+    }
+
+    IEnumerator Flicker()
+    {
+        isFlickering = true;
+
+        this.GetComponent<Light>().enabled = false; // turns off the light
+
+        flickerDelay = Random.Range(rangeX, rangeY);
+
+        yield return new WaitForSeconds(flickerDelay); // delay between flickers
+
+        this.GetComponent<Light>().enabled = true; // turns on the light
+
+        isFlickering = false;
+    }
+}
