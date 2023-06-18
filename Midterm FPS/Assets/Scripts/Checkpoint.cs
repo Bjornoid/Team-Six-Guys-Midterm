9783fbc6c2fbf6bc6c8a5d2512ff1451e45c0d1e
LeviBaker1 <126530015+LeviBaker1@users.noreply.github.com>
2023-06-18 02:01:04 -0400

    Added Checkpoints
    Added checkpoint platforms

---
+++ Midterm FPS/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && gameManager.instance.playerSpawnPosition.transform.position != transform.position)
+        {
+            gameManager.instance.playerSpawnPosition.transform.position = transform.position;
+            StartCoroutine(flashColor());
+        }
+    }
+    IEnumerator flashColor()
+    {
+        model.material.color = Color.red;
+        //gameManager.instance.checkpointpopUp.SetActive(true);
+        yield return new WaitForSeconds(0.4f);
+        model.material.color = Color.white;
+        //gameManager.instance.checkpointpopUp.SetActive(false);
+    }
+}
