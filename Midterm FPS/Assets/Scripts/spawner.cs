66672782d6b19e93d0f528dfcb6e00b0805647fd
James Green <JDGreen4@student.fullsail.edu>
2023-06-17 20:18:11 -0400

    Created spawner script and prefab
    Remade the spawner script and prefab from class example

---
+++ Midterm FPS/Assets/Scripts/spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPos;
+    [SerializeField] int numToSpawn;
+    [SerializeField] float timeBetweenSpawns;
+    [SerializeField] GameObject spawnObj;
+    int numSpawned;
+    bool playerInRange;
+    bool isSpawning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && !isSpawning && numSpawned < numToSpawn)
+        {
+            StartCoroutine(spawn());
+        }
+    }
+
+    IEnumerator spawn()
+    {
+        isSpawning = true;
+        Instantiate(spawnObj, spawnPos[Random.Range(0, spawnPos.Length)].position, transform.rotation);
+        numSpawned++;
+        yield return new WaitForSeconds(timeBetweenSpawns);
+        isSpawning = false;
+    }
+}
