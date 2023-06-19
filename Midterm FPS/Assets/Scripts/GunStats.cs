ae2b5a2f6c342682d15ed67c11dfb06af40f4b97
Bjorn Nordenflycht <121911108+Bjornoid@users.noreply.github.com>
2023-06-19 19:22:48 -0400

    Ammo System and max ammo pickup
    Ammo system works and you can reload. Ammo pickup just gives all your guns max ammo.

--- Midterm FPS/Assets/Scripts/GunStats.cs
+++ Midterm FPS/Assets/Scripts/GunStats.cs
@@ -8,6 +8,11 @@ public class GunStats : ScriptableObject
     public int shootDist;
     public float shootRate;
     public int shootDmg;
+    public int magAmmoCurr;
+    public int magAmmoMax;
+    public int reserveAmmoCurr;
+    public int reserveAmmoMax;
+    public float reloadTime;
 
     public string gunName;
 
