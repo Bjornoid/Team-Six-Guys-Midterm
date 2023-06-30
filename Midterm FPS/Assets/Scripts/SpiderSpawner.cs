6351b88861d550841c6e4d78cb30e800eced109b
LeviBaker1 <126530015+LeviBaker1@users.noreply.github.com>
2023-06-30 17:18:34 -0400

    Added a spider spawner
    Spawns spiders continously until it dies.

---
+++ Midterm FPS/Assets/Scripts/SpiderSpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderSpawner : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] Transform[] spawnPos;
+    [SerializeField] float spawnSpeed;
+    [SerializeField] int numSpawned;
+    [SerializeField] GameObject spawnEntity;
+    [SerializeField] int hp;
+    bool isSpawning;
+    bool playerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.instance.UpdateGameGoal(numSpawned);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange= true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(playerInRange && !isSpawning) 
+        {
+            StartCoroutine(spawnContinous());
+        }
+    }
+    IEnumerator spawnContinous()
+    {
+        isSpawning= true;
+        Instantiate(spawnEntity, spawnPos[Random.Range(0,spawnPos.Length)].position, transform.rotation);
+        numSpawned++;
+        yield return new WaitForSeconds(spawnSpeed);
+        isSpawning= false;
+    }
+    public void takeDamage(int dmg)
+    {
+        hp -= dmg;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashColor());
+        }
+        IEnumerator flashColor()
+        {
+            model.material.color = Color.blue;
+            yield return new WaitForSeconds(0.1f);
+            model.material.color = Color.white;
+        }
+    }
+   
+}
