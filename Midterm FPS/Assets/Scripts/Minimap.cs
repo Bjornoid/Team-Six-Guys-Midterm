5fdc6f938362959bb15f6c1910958e09c11e1209
Bjorn Nordenflycht <bjornnorden17@gmail.com>
2023-07-03 22:29:48 -0400

    Mini Map Alpha Ready
    mini map now is ready for alpha milestone

--- Midterm FPS/Assets/Scripts/Minimap.cs
+++ Midterm FPS/Assets/Scripts/Minimap.cs
@@ -6,8 +6,6 @@ public class Minimap : MonoBehaviour
 {
     private Transform player;
 
-    public Transform minimapOverlay;
-
     private GameObject[] enemies;
     void Start()
     {
@@ -19,20 +17,5 @@ public class Minimap : MonoBehaviour
     void Update()
     {
         transform.position = player.position + Vector3.up * 5f;
-        HandleEnemyVisible();
-        RotateOverlay();
-    }
-
-    private void HandleEnemyVisible()
-    {
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            enemies[i].SetActive(Vector3.Angle(player.forward, enemies[i].transform.position - player.position) <= 100);
-        }
-    }
-
-    private void RotateOverlay()
-    {
-        minimapOverlay.localRotation = Quaternion.Euler(0, 0, -player.eulerAngles.y - 100);
     }
 }
