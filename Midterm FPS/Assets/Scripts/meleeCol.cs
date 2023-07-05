65b4be9da5c95ff7b4d0d4b0228a098248272206
James Green <JDGreen4@student.fullsail.edu>
2023-07-05 03:40:05 -0400

    Updated meleeCol with poison option
    Adds poison functionality to meleeCol

--- Midterm FPS/Assets/Scripts/meleeCol.cs
+++ Midterm FPS/Assets/Scripts/meleeCol.cs
@@ -18,7 +18,19 @@ public class meleeCol : MonoBehaviour
         if (damageable != null)
         {
             damageable.takeDamage(damage);
+            if (isPoison)
+            {
+                StartCoroutine(poisoned(damageable));
+            }
         }
+    }
 
+    IEnumerator poisoned(IDamage damagable)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            damagable.takeDamage(1);
+            yield return new WaitForSeconds(0.7f);
+        }
     }
 }
