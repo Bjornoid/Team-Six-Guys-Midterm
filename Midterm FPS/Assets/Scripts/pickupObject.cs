64fa856317d12d5ecdfdb2f15c970909a22b4d3a
James Green <JDGreen4@student.fullsail.edu>
2023-07-08 16:22:16 -0400

    Created pickup trigger gameObject
    Created pickup trigger game object and script which uses unity events to let the player pick up objects with E

---
+++ Midterm FPS/Assets/Scripts/pickupObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class pickupObject : MonoBehaviour
+{
+    bool inRange;
+    [SerializeField] UnityEvent action;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            if (Input.GetButtonDown("Pickup"))
+            {
+                action.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = true;
+            gameManager.instance.pickupPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
+            gameManager.instance.pickupPrompt.SetActive(false);
+        }
+    }
+}
