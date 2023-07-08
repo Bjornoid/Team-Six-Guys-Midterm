93d5eab9769ebfd640ce579a708a59a8981fb5d6
James Green <JDGreen4@student.fullsail.edu>
2023-07-08 18:53:19 -0400

    Organized prefabs

--- Midterm FPS/Assets/Scripts/GunParts.cs
+++ Midterm FPS/Assets/Scripts/GunParts.cs
@@ -7,18 +7,15 @@ public class GunParts : MonoBehaviour
     [SerializeField] GameObject gunPart;
     [SerializeField] GameObject gunBuilt;
 
-    private void OnTriggerEnter(Collider other)
+    public void pickup()
     {
-        if (other.CompareTag("Player"))
-        {
-            gameManager.instance.partList.Add(gunPart);
-            gunPart.SetActive(false);
+        gameManager.instance.partList.Add(gunPart);
+        gunPart.SetActive(false);
 
-            int fullGun = 3;
-            if (gameManager.instance.partList.Count == fullGun)
-            {
-                gunBuilt.SetActive(true);
-            }
+        int fullGun = 3;
+        if (gameManager.instance.partList.Count == fullGun)
+        {
+            gunBuilt.SetActive(true);
         }
     }
 }
