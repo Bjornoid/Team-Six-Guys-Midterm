2ef6ce46ce1217b82068f020e02b48b9ec23a033
Bjorn Nordenflycht <bjornnorden17@gmail.com>
2023-07-09 14:01:17 -0400

    tutorial upgrade

--- Midterm FPS/Assets/Scripts/KeyAndLockTrigger.cs
+++ Midterm FPS/Assets/Scripts/KeyAndLockTrigger.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class KeyAndLockTrigger : MonoBehaviour, IPickup
 {
     [Header("----- Game Objects -----")]
     public GameObject theKey;
     public GameObject theLock;
-
+    [SerializeField] UnityEvent action;
     void Start()
     {
         gameManager.instance.updateLockCount(1);
     }
-    
+
     public void pickup()
     {
         gameManager.instance.playerScript.pickupKey();
         theKey.SetActive(false);
         theLock.SetActive(false);
         gameManager.instance.updateLockCount(-1);
+        if (gameManager.instance.getLocksRemaining() == 0)
+            action.Invoke();
     }
+
 }
