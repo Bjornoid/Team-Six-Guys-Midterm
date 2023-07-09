46fd3e253cf3dfaa56f4427b354f2a071a7a6dc6
ShaunAnderson17 <118745439+ShaunAnderson17@users.noreply.github.com>
2023-07-09 18:07:36 -0400

    added brightness

--- Midterm FPS/Assets/Scripts/Brightness.cs
+++ Midterm FPS/Assets/Scripts/Brightness.cs
@@ -1,31 +1,31 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using UnityEngine.Rendering.PostProcessing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
-//public class Brightness : MonoBehaviour
-//{
-//    public Slider brightnessSlider;
-//    public PostProcessLayer layer;
-//    public PostProcessProfile brightness;
-//    AutoExposure exposure;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        brightness.TryGetSettings(out exposure);
-//        AdjustBrightness(brightnessSlider.value);
-//    }
+public class Brightness : MonoBehaviour
+{
+    public Slider brightnessSlider;
+    public PostProcessLayer layer;
+    public PostProcessProfile brightness;
+    AutoExposure exposure;
+    // Start is called before the first frame update
+    void Start()
+    {
+        brightness.TryGetSettings(out exposure);
+        AdjustBrightness(brightnessSlider.value);
+    }
 
-//    public void AdjustBrightness(float val)
-//    {
-//        if(val != 0)
-//        {
-//            exposure.keyValue.value = val;
-//        }
-//        else
-//        {
-//            exposure.keyValue.value = 0.05f;
-//        }
-//    }
-//}
+    public void AdjustBrightness(float val)
+    {
+        if (val != 0)
+        {
+            exposure.keyValue.value = val;
+        }
+        else
+        {
+            exposure.keyValue.value = 0.05f;
+        }
+    }
+}
